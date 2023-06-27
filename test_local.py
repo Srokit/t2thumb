650662313526685377be69b567873242b0942de3
Stan <stan@stansa.dev>
2023-06-27 09:44:18 -0700

    c

--- test_local.py
+++ test_local.py
@@ -12,14 +12,17 @@ USE_FIREFOX = False
 
 IMGS_DIR = "imgs"
 
+NUM_PAGE_REFRESHES = 10
+
 def main():
     if os.path.exists(IMGS_DIR):
         os.system(f"rm -rf {IMGS_DIR}")
     os.mkdir(IMGS_DIR)
     client = T2TClient(is_firefox=USE_FIREFOX)
-    for i, img in enumerate(client.for_each_img()):
-        out_path = os.path.join(IMGS_DIR, f"explore_img_{i}.png")
-        with open(out_path, "wb") as f: f.write(img)
+    for refresh_i in range(NUM_PAGE_REFRESHES):
+        for i, img in enumerate(client.for_each_img()):
+            out_path = os.path.join(IMGS_DIR, f"img_{refresh_i}_{i}.png")
+            with open(out_path, "wb") as f: f.write(img)
 
 if __name__ == "__main__":
     main()
--- test_s3_buck.py
+++ test_s3_buck.py
@@ -14,18 +14,21 @@ USE_FIREFOX = False
 IMGS_DIR = "imgs"
 S3_BUCKET = "t2thumb-test"
 
+NUM_REFRESHES = 10
+
 def main():
     s3 = boto3.client('s3')
     if os.path.exists(IMGS_DIR):
         os.system(f"rm -rf {IMGS_DIR}")
     os.mkdir(IMGS_DIR)
     client = T2TClient(is_firefox=USE_FIREFOX)
-    for i, img in enumerate(client.for_each_img()):
-        out_path = os.path.join(IMGS_DIR, f"explore_img_{i}.png")
-        with open(out_path, "wb") as f: f.write(img)
-        s3.upload_file(out_path, S3_BUCKET, out_path)
-        # rm file
-        os.remove(out_path)
+    for refresh_i in range(NUM_REFRESHES):
+        for i, img in enumerate(client.for_each_img()):
+            out_path = os.path.join(IMGS_DIR, f"img_{refresh_i}_{i}.png")
+            with open(out_path, "wb") as f: f.write(img)
+            s3.upload_file(out_path, S3_BUCKET, out_path)
+            # rm file
+            os.remove(out_path)
 
 if __name__ == "__main__":
     main()
