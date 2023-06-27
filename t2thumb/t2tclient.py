9b364e37c7c392a0fffbfc99bacaf51c1a5a6e5a
Stan <stan@stansa.dev>
2023-06-27 09:19:55 -0700

    c

--- t2thumb/t2tclient.py
+++ t2thumb/t2tclient.py
@@ -1,5 +1,7 @@
 from typing import Generator
 
+import cv2
+import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -30,10 +32,27 @@ class T2TClient:
             self._sel_driver = webdriver.Firefox(opts)
         else:
             self._sel_driver = webdriver.Chrome()
+        self._print_init_setup()
 
     def for_each_img(self) -> Generator[bytes, None, None]:
         """Download all the images from TikTok and yield them as bytes.
         """
         self._sel_driver.get(f"{BASE_URL}/{self._page}")
         eles = self._sel_driver.find_elements(By.XPATH, IMGS_XPATH)
-        for ele in eles: yield ele.screenshot_as_png
+        for ele in eles:
+            data = ele.screenshot_as_png
+            img = cv2.imdecode(
+                np.frombuffer(data, np.uint8), cv2.IMREAD_COLOR)
+            # Crop image
+            img = img[:self._img_crop_to_size[0], :self._img_crop_to_size[1]]
+            # Resize image
+            img = cv2.resize(img, self._img_size_out, cv2.INTER_CUBIC)
+            yield cv2.imencode(".png", img)[1].tobytes()
+
+    def _print_init_setup(self) -> None:
+        "Print the information about how this client is setup."
+        print("T2TClient setup:")
+        print(f"  Using page: {self._page}")
+        print(f"  Cropping images to: {self._img_crop_to_size}")
+        print(f"  Resizing images to: {self._img_size_out}")
+        print(f"  Using {'Firefox' if isinstance(self._sel_driver, webdriver.Firefox) else 'Chrome'}")
